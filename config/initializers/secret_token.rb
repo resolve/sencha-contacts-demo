594aeaf63ff75e5b62a08146ea6bfaee9ce63dde
Mark Haylock <mark@resolvedigital.co.nz>
2011-05-25 10:59:04 +1200

    Initial commit after rails generate.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SenchaContactsDemo::Application.config.secret_token = '9e6ca643eabcd4a6735f33b1dfda2aa2175cf56d37738929e8471e17aba19683355a6ce45708006901cc427faaa608d846918442874188453a0abefadb2dd163'
