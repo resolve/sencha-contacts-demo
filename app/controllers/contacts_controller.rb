37472c76e0557cde315b85fffcd89f1b8cdf651e
Mark Haylock <mark@resolvedigital.co.nz>
2011-05-25 11:20:25 +1200

    Updated Contacts controller to handle Sencha XML requests.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,7 +6,7 @@ class ContactsController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @contacts }
+      format.xml  { render :xml => @contacts, :dasherize => false }
     end
   end
 
@@ -40,18 +40,22 @@ class ContactsController < ApplicationController
   # POST /contacts
   # POST /contacts.xml
   def create
-    @contact = Contact.new(params[:contact])
-
     respond_to do |format|
-      if @contact.save
-        format.html { redirect_to(@contact, :notice => 'Contact was successfully created.') }
-        format.xml  { render :xml => @contact, :status => :created, :location => @contact }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @contact.errors, :status => :unprocessable_entity }
-      end
+      format.html {
+        @contact = Contact.new(params[:contact])
+        if @contact.save
+          redirect_to(@contact, :notice => 'Contact was successfully created.')
+        else
+          render :action => 'new'
+        end
+      }
+      format.xml {
+        @contact = parse_sencha_xml
+        render :xml => @contact, :status => :created, :dasherize => false
+      }
     end
   end
+  
 
   # PUT /contacts/1
   # PUT /contacts/1.xml
@@ -79,5 +83,37 @@ class ContactsController < ApplicationController
       format.html { redirect_to(contacts_url) }
       format.xml  { head :ok }
     end
+  end
+  
+  def parse_sencha_xml
+    p_hash = params[:xmlData][:contact]
+    return nil if p_hash.nil? || p_hash.empty?
+    # call update if the remote_id is populated, means it's already in the db
+    if p_hash['remote_id'].to_i > 0
+      return update_record(p_hash)
+    end
+    contact = Contact.create!(parse_contact_params(p_hash))
+    return contact
+  end
+
+  def update_record(p_hash)
+    id = params[:xmlData][:contact]['remote_id']
+    c = Contact.find(id)
+    update_params = parse_contact_params(p_hash)
+    c.update_attributes(update_params)
+    return c
+  end
+
+  def parse_contact_params(p_hash)
+    contact_hash = Hash.new
+    attr = Contact.new.attributes
+    p_hash.each do |key, value|
+      if (key == 'id' || key == 'remote_id' )
+      # skip, this is the device PK or the db PK.  The db PK has already been captured
+      elsif attr.has_key?(key)
+        contact_hash[key] = value
+      end
+    end
+    return contact_hash
   end
 end
